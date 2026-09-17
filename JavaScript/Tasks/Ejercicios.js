da00a31d9fa105926dcbde22b65fb0d351ae8b50
Diego Alejandro Pena Usma <diego.pena01@uceva.edu.co>
2026-09-16 22:22:45 -0500

    feat: solucionar ejercicio 5, filtrar empleados

--- JavaScript/Tasks/Ejercicios.js
+++ JavaScript/Tasks/Ejercicios.js
@@ -109,12 +109,22 @@ console.log(Object.entries(estudiante));
  * Obtén un nuevo arreglo que contenga únicamente los empleados 
  * cuyo salario sea mayor o igual a 4000.
  */
+console.log("==========EJERCICIO 5===========");
+
 const empleados = [
     { nombre: "Ana", salario: 3000 },
     { nombre: "Luis", salario: 5000 },
     { nombre: "Pedro", salario: 2500 },
     { nombre: "Sara", salario: 7000 }
 ];
+
+const empleadosFiltrados = 
+empleados.filter(function(empleado){
+    return empleado.salario >= 4000;
+});
+
+console.log(empleadosFiltrados);
+
 
 /**
  * TODO: EJERCICIO 6 - Estadísticas de Ventas
