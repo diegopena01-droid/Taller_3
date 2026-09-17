784f22f355682632e3a0ab833bebf08d2eb14d4c
Cristhian Pulido <cristhianpulidor26@gmail.com>
2026-09-16 23:59:06 -0500

    feat: desestructurar computador

--- JavaScript/Tasks/Ejercicios.js
+++ JavaScript/Tasks/Ejercicios.js
@@ -243,12 +243,21 @@ console.log("Copia por spread: ", copiaSpread);
  *  - Asigna un valor por defecto a una propiedad llamada tarjetaGrafica.
  */
 
+console.log("==========EJERCICIO 9===========");
+
 const computador = {
     marca: "Lenovo",
     procesador: "Intel i7",
     ram: 16,
     almacenamiento: 512
 };
+
+const { marca, ram, almacenamiento: ssd, tarjetaGrafica = "NVIDIA" } = computador;
+
+console.log("Marca: ", marca);
+console.log("RAM: ", ram);
+console.log("SSD: ", ssd);
+console.log("Tarjeta Gráfica: ", tarjetaGrafica);
 
 /**
  * TODO: EJERCICIO 10 - Sistema de Gestión de Estudiantes
